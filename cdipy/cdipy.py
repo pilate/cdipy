fda5bcf6f9b5a295262a841766b8ebdf226917a2
Pilate <noptical@gmail.com>
2020-08-14 09:39:58 -0700

    change json priority list

--- cdipy/cdipy.py
+++ cdipy/cdipy.py
@@ -15,9 +15,12 @@ import types
 from pyee import EventEmitter
 
 try:
-    import ujson as json
+    import simdjson as json
 except:
-    import json
+    try:
+        import ujson as json
+    except:
+        import json
 
 import aiohttp
 import websockets
@@ -49,7 +52,7 @@ async def download_data():
             new_path = CACHE_FOLDER / response.url.name
             open(new_path, "w+b").write(await response.read())
             logger.debug(f"Wrote {new_path}")
-            
+
 if not os.path.exists(CACHE_FOLDER):
     os.mkdir(CACHE_FOLDER, mode=0o744)
 
@@ -74,11 +77,11 @@ def create_signature(params):
             default = None
 
         new_param = inspect.Parameter(
-            name=param["name"], 
+            name=param["name"],
             kind=inspect.Parameter.POSITIONAL_OR_KEYWORD,
             default=default)
 
-        new_params.append(new_param)   
+        new_params.append(new_param)
 
     new_params.sort(key=lambda p: bool(p.default), reverse=True)
 
@@ -98,7 +101,7 @@ def wrap_factory(command_name, signature):
     """
         Creates a new function that can be used as a domain method
     """
-    
+
     async def wrapper(self, *args, **kwargs):
         """
             - Validate method arguments against <signature>
@@ -129,7 +132,7 @@ class Devtools(EventEmitter):
             Wait for a specific event to fire before returning
         """
         future = asyncio.get_event_loop().create_future()
-        
+
         def update_future(*args, **kwargs):
             future.set_result((args, kwargs))
 
@@ -218,7 +221,7 @@ class ChromeDevTools(Devtools):
 
 
     async def connect(self):
-        self.websocket = await websockets.client.connect(self.ws_uri, 
+        self.websocket = await websockets.client.connect(self.ws_uri,
             max_size=2**32, read_limit=2**32, max_queue=2**32)
         self.task = asyncio.ensure_future(self._recv_loop())
 
