e9027c44c1b6f28f4031b1b7659b9ffcc0570c6e
pilate <noptical@gmail.com>
2021-03-11 10:01:52 -0800

    update aiohttp

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.1",
     packages=find_packages(),
     install_requires=[
-        "aiohttp==3.5.1", 
+        "aiohttp<3.8.0", 
         "pyee==5.0.0",
         "websockets==6.0"
     ],
