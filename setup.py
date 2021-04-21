2c8df8bb8cce5b169594fdca16a20d20bf3be7d7
Pilate <noptical@gmail.com>
2021-04-21 12:46:14 -0700

    freeze versions

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ setup(
     version="0.1",
     packages=find_packages(),
     install_requires=[
-        "aiohttp<3.8.0", 
+        "aiohttp<3.8.0",
         "pyee==5.0.0",
-        "websockets==8.1"
+        "websockets<8.2"
     ],
     author="Pilate",
     author_email="Pilate@pilate.es",
